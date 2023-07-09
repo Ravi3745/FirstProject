f047cda7cba53d15a00948f4ee2cfa06fe7bea69
Kant Sharma <ravi.kant-sharma@capgemini.com>
2023-07-09 12:32:38 +0530

    static files setup

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
+// require express
 const express= require('express');
 const app=express();
 const port=5000;
+const path=require('path');
+
+// this one is for setting up view engine 
 app.set('view engine','ejs');
 app.set('views','./views')
 
+// for using static files css images js 
+// app.use(express.static(path.join(__dirname,'assets')));
+// app.use(express.static(path.join(__dirname,'assets'))); this is not working in my case due to mime type so have to use use other way of routing
+
+app.get('/assets/css/layout.css',function(req,res){
+    res.set('Content-type','text/css');
+    res.sendFile(path.join(__dirname,'assets','css','layout.css'));
+})
+
+// similarily i have to do the same for js file and image file
+
+app.get('/assets/js/layout.js',function(req,res){
+    res.set('Content-type','text/javascript');
+    res.sendFile(path.join(__dirname,'assets','js','layout.js'));
+
+})
+// i dont know what is going on this mim type issue because of this i have to include every thing here
+app.get('/assets/css/home.css', function(req, res) {
+    res.type('text/css');
+    res.sendFile(path.join(__dirname, 'assets', 'css', 'home.css'));
+  });
 
 
+// this one is for setting up layouts
+const expressLayouts=require('express-ejs-layouts');
+app.use(expressLayouts);
+
+// this one is for including style ans script of individual pages
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+// controllers
 app.use('/',require('./routes'));
 
+
+
+
+// firing server
 app.listen(port,function(error){
     if(error){
         console.log(`There is an error found in running the server that is : ${error}`);

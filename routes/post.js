fde511f623daed913ee915238183a1c750422e4c
Kant Sharma <ravi.kant-sharma@capgemini.com>
2023-07-18 22:57:48 +0530

    unauthenticated user can not fill form

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const router = require('.');
-
+const passport=require('passport');
 const route=express.Router();
 
 const postController=require('../controllers/post_controller');
 
-route.post('/create',postController.create);
+route.post('/create',passport.checkAuthentication,postController.create);
 
 module.exports=route;
